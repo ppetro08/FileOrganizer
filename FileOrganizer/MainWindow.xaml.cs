f77bf875b1f243c4ddbb0a36b84c81c3cef47174
ppetro08 <ppetro08@gmail.com>
2014-10-08 23:55:06 -0400

    Started Hiearchal View

--- FileOrganizer/MainWindow.xaml.cs
+++ FileOrganizer/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,13 +14,34 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace FileOrganizer {
+namespace FileOrganizer
+{
    /// <summary>
    /// Interaction logic for MainWindow.xaml
    /// </summary>
-   public partial class MainWindow : Window {
-      public MainWindow() {
+   public partial class MainWindow : Window
+   {
+      public List<TreeViewModel> TheList { get; set; }
+
+      public MainWindow()
+      {
          InitializeComponent();
+      }
+
+      // Runs when window opens
+      private void Window_Loaded(object sender, RoutedEventArgs e)
+      {
+         TheList = new List<TreeViewModel>();
+
+         TheList.Add(new TreeViewModel(Name = "Parent"));
+
+         for (int i = 1; i < 4; i++)
+         {
+            TheList[0].Children.Add(new TreeViewModel("Name" + i, "Path" + i));
+         }
+         TheList[0].Children.Add(new TreeViewModel("Name" + 4, "Path" + 4));
+
+         this.DataContext = this;
       }
    }
 }
---
+++ FileOrganizer/TreeViewModel.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileOrganizer
+{
+   public class TreeViewModel : INotifyPropertyChanged
+   {
+      public TreeViewModel(string name)
+      {
+         Name = name;
+         Children = new List<TreeViewModel>();
+      }
+
+      public TreeViewModel(string name, string fullPath)
+      {
+         Name = name;
+         FullPath = fullPath;
+         Children = new List<TreeViewModel>();
+      }
+
+      public string Name { get; private set; }
+      public string FullPath { get; private set; }
+      public List<TreeViewModel> Children { get; private set; }
+
+      bool isChecked = false;
+      TreeViewModel _parent;
+
+      public bool IsChecked
+      {
+         get { return isChecked; }
+         set
+         {
+            isChecked = value;
+            NotifyPropertyChanged("IsChecked");
+         }
+      }
+
+      void Initialize()
+      {
+         foreach (TreeViewModel child in Children)
+         {
+            child._parent = this;
+            child.Initialize();
+         }
+      }
+
+      public static List<TreeViewModel> setTree(TreeViewModel tree)
+      {
+         List<TreeViewModel> treeView = new List<TreeViewModel>();
+         treeView.Add(tree);
+         tree.Initialize();
+
+         return treeView;
+      }
+
+      public static List<string> GetSelected()
+      {
+         List<string> selected = new List<string>();
+
+         //select = recursive method to check each tree view item for selection (if required)
+
+         return selected;
+      }
+
+      #region Notify
+      public event PropertyChangedEventHandler PropertyChanged;
+      protected void NotifyPropertyChanged(string strPropertyName)
+      {
+         if (PropertyChanged != null)
+            PropertyChanged(this, new PropertyChangedEventArgs(strPropertyName));
+      }
+      #endregion
+   }
+}
