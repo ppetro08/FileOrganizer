358a003c329aeb6522af4bf179ccdcf8bc5e9e8d
ppetro08 <ppetro08@gmail.com>
2014-10-20 23:07:15 -0400

    syncing files

--- FileOrganizer/Show.cs
+++ FileOrganizer/Show.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -97,8 +97,8 @@ namespace FileOrganizer
             }
 
             // Changes the name of the folder to include the title of the show
-            //if (i < mark - 1)
-            //   folder = folder + " " + UppercaseFirst(splittv[i]);
+            if (i < mark - 1 && i != 0)
+               folder = folder + " " + UppercaseFirst(splittv[i]);
          }
 
          if (!file.Contains("part", StringComparison.InvariantCultureIgnoreCase))
